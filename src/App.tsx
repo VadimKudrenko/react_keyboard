891fe04f171048845385ada517ea412d78a1bd3e
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-03-10 13:13:45 +0200

    add soultion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  keyPressed: string;
+};
+
+export class App extends React.Component {
+  state: State = {
+    keyPressed: '',
+  };
+
+  handlerKeydown = (event: KeyboardEvent) => {
+    this.setState({ keyPressed: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handlerKeydown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handlerKeydown);
+  }
+
+  render() {
+    const { keyPressed } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {keyPressed
+            ? `The last pressed key is [${keyPressed}]`
+            : 'Nothing was pressed yet'}
+        </p>
+      </div>
+    );
+  }
+}
